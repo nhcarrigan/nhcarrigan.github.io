d6d11fe219d3b0a4c4b963d5fc1ed43535c42ea1
Naomi Carrigan <nhcarrigan@gmail.com>
2022-07-14 11:38:24 -0700

    fix(tools): remove unnecessary test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('homepage');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('homepage app is running!');
-  });
 });
