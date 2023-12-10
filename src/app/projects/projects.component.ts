e14a8c56fd0a151c8c1d3f82da27f948ce462588
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-10 09:07:01 -0800

    feat: add personal projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-
+  public projects = [
+    {
+      name: 'Becca Lyria',
+      link: 'https://becca.nhcarrigan.com',
+      title: 'Moderation/Community Management Discord Bot',
+      avatar: 'becca.png',
+      since: 'May 2020',
+    },
+    {
+      name: 'Beccalia',
+      link: 'https://nhcarrigan.itch.io',
+      title: 'RPGMaker Game Series',
+      avatar: 'beccalia.png',
+      since: 'November 2021',
+    },
+    {
+      name: 'Melody Iuvo',
+      link: 'https://github.com/naomi-lgbt/melody-iuvo',
+      title: 'Personal Assistant Discord Bot',
+      avatar: 'melody.png',
+      since: 'August 2023',
+    },
+  ];
 }
