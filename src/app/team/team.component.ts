d8d783d76f48d19179d0c4e7dda4958fe063a2d7
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-10 10:37:11 -0800

    feat: build out team component
    It's kinda empty lol

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './team.component.html',
-  styleUrl: './team.component.css'
+  styleUrl: './team.component.css',
 })
 export class TeamComponent {
-
+  public employees = [
+    {
+      name: 'Naomi Carrigan',
+      link: 'https://naomi.lgbt',
+      title: 'Owner/Sole Proprietor',
+      avatar: 'naomi.png',
+      since: 'December 2020',
+    },
+  ];
 }
